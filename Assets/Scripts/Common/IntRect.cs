4364fcf71822df1cf85d3d3d56e0b8b16120b675
GromHoll <gromholl@gmail.com>
2014-11-14 00:29:17 +0300

    Implemented and Tested IntRect

--- Assets/Scripts/Common/IntRect.cs
+++ Assets/Scripts/Common/IntRect.cs
@@ -10,5 +10,16 @@ namespace TrappedGame {
             point1 = new IntVector2(x1, y1);
             point2 = new IntVector2(x2, y2);
         }
+
+        public bool Contains(IntVector2 vec) {
+            return Contains(vec.x, vec.y);
+        }
+
+        public bool Contains(int x, int y) {
+            return Math.Min(point1.x, point2.x) <= x 
+                && Math.Max(point1.x, point2.x) >= x
+                && Math.Min(point1.y, point2.y) <= y 
+                && Math.Max(point1.y, point2.y) >= y;
+        }
     }
 }
---
+++ Assets/Tests/Editor/Common/IntRectTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using TrappedGame;
+using System;
+
+namespace TrappedUnitTests {
+    [TestFixture()]
+    public class IntRectTest {
+        [Test()]
+        public void ContainsTest() {
+            // 3 . . . . . . *
+            // 2 . x x x * x .
+            // 1 . x * . . x *
+            // 0 . x x x x x . 
+            //-1 * . . * . . . 
+            //  -2-1 0 1 2 3 4
+            IntRect rect = new IntRect(-1, 0, 3, 2);
+
+            Assert.IsTrue(rect.Contains(0, 1));
+            Assert.IsTrue(rect.Contains(2, 2));
+            
+            Assert.IsFalse(rect.Contains(-2, -1));
+            Assert.IsFalse(rect.Contains(1, -1));
+            Assert.IsFalse(rect.Contains(4, 3));
+            Assert.IsFalse(rect.Contains(4, 4));
+        }
+    }
+}
+
