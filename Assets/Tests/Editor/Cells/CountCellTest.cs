ec922dd0f242df29e842550add24ac2d6a84a850
GromHoll <gromholl@gmail.com>
2014-11-13 23:43:15 +0300

    fixed namespace in Test

--- Assets/Tests/Editor/Cells/CountCellTest.cs
+++ Assets/Tests/Editor/Cells/CountCellTest.cs
@@ -2,73 +2,76 @@ using NUnit.Framework;
 using UnityEngine;
 using System;
 
-[TestFixture]
-public class CountCellTest {
-    [Test]
-    public void CreateTest() {
-        var cell = new CountCell(1, 2, CellType.UNKNOWN);
 
-        Assert.AreEqual(1, cell.GetX());
-        Assert.AreEqual(2, cell.GetY());
-        Assert.AreEqual(CellType.UNKNOWN, cell.GetCellType());
-        
-        Assert.IsFalse(cell.IsOn());   
-        Assert.IsFalse(cell.IsBocked());
-        Assert.IsFalse(cell.IsDeadly());
-    }
-    
-    [Test]
-    public void NextTickTest() {
-        var on = 3;
-        var off = 2;
-        var current = 1;
-       
-        var cell = new CountCell(0, 0, CellType.UNKNOWN, on, off, current, false);
+namespace TrappedUnitTests {
+    [TestFixture]
+    public class CountCellTest {
+        [Test]
+        public void CreateTest() {
+            var cell = new CountCell(1, 2, CellType.UNKNOWN);
 
-        Assert.IsFalse(cell.IsOn());
+            Assert.AreEqual(1, cell.GetX());
+            Assert.AreEqual(2, cell.GetY());
+            Assert.AreEqual(CellType.UNKNOWN, cell.GetCellType());
+            
+            Assert.IsFalse(cell.IsOn());   
+            Assert.IsFalse(cell.IsBocked());
+            Assert.IsFalse(cell.IsDeadly());
+        }
+        
+        [Test]
+        public void NextTickTest() {
+            var on = 3;
+            var off = 2;
+            var current = 1;
+           
+            var cell = new CountCell(0, 0, CellType.UNKNOWN, on, off, current, false);
 
-        cell.NextTick();
-        Assert.IsTrue(cell.IsOn());
-        
-        cell.NextTick();
-        Assert.IsTrue(cell.IsOn());
-        
-        cell.NextTick();
-        Assert.IsTrue(cell.IsOn());
-        
-        cell.NextTick();
-        Assert.IsFalse(cell.IsOn());
-        
-        cell.NextTick();
-        Assert.IsFalse(cell.IsOn());
-        
-        cell.NextTick();
-        Assert.IsTrue(cell.IsOn());
-    }
+            Assert.IsFalse(cell.IsOn());
 
-    [Test]
-    public void BackTickTest() {
-        var on = 1;
-        var off = 2;
-        var current = 0;
-        
-        var cell = new CountCell(0, 0, CellType.UNKNOWN, on, off, current, true);
-        
-        Assert.IsTrue(cell.IsOn());
-        
-        cell.BackTick();
-        Assert.IsFalse(cell.IsOn());
-        
-        cell.BackTick();
-        Assert.IsFalse(cell.IsOn());
-        
-        cell.BackTick();
-        Assert.IsTrue(cell.IsOn());
-        
-        cell.BackTick();
-        Assert.IsFalse(cell.IsOn());
-        
-        cell.BackTick();
-        Assert.IsFalse(cell.IsOn());
+            cell.NextTick();
+            Assert.IsTrue(cell.IsOn());
+            
+            cell.NextTick();
+            Assert.IsTrue(cell.IsOn());
+            
+            cell.NextTick();
+            Assert.IsTrue(cell.IsOn());
+            
+            cell.NextTick();
+            Assert.IsFalse(cell.IsOn());
+            
+            cell.NextTick();
+            Assert.IsFalse(cell.IsOn());
+            
+            cell.NextTick();
+            Assert.IsTrue(cell.IsOn());
+        }
+
+        [Test]
+        public void BackTickTest() {
+            var on = 1;
+            var off = 2;
+            var current = 0;
+            
+            var cell = new CountCell(0, 0, CellType.UNKNOWN, on, off, current, true);
+            
+            Assert.IsTrue(cell.IsOn());
+            
+            cell.BackTick();
+            Assert.IsFalse(cell.IsOn());
+            
+            cell.BackTick();
+            Assert.IsFalse(cell.IsOn());
+            
+            cell.BackTick();
+            Assert.IsTrue(cell.IsOn());
+            
+            cell.BackTick();
+            Assert.IsFalse(cell.IsOn());
+            
+            cell.BackTick();
+            Assert.IsFalse(cell.IsOn());
+        }
     }
 }
