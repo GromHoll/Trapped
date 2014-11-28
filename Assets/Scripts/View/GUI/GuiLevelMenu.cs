4a1b3c1ebce2c4434c21392b886e0db1f5f7e547
Andrew <empire256@gmail.com>
2014-11-28 16:12:59 +0300

    Added Main Menu scene

--- Assets/Scripts/View/GUI/GuiLevelMenu.cs
+++ Assets/Scripts/View/GUI/GuiLevelMenu.cs
@@ -3,28 +3,8 @@ using System.Collections;
 
 public class GuiLevelMenu : MonoBehaviour {
 
-	void OnGUI() {
-		GUI.Box (new Rect(10, 10, 100, 150), "Levels");
-
-		if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1")) {
-			PlayerPrefs.SetString("CurrentLevel", "Levels/Level1");
-			Application.LoadLevel("Level");
-		}
-
-		if (GUI.Button(new Rect(20, 70, 80, 20), "Level 2")) {
-			PlayerPrefs.SetString("CurrentLevel", "Levels/Level2");
-			Application.LoadLevel("Level");
-		}
-
-		if (GUI.Button(new Rect(20, 100, 80, 20), "Level 3")) {
-			PlayerPrefs.SetString("CurrentLevel", "Levels/Level3");
-			Application.LoadLevel("Level");
-		}
-
-		if (GUI.Button(new Rect(20, 130, 80, 20), "Level 4")) {
-			PlayerPrefs.SetString("CurrentLevel", "Levels/Level4");
-			Application.LoadLevel("Level");
-		}
+	public void LoadLevel(string levelName) {
+		PlayerPrefs.SetString("CurrentLevel", levelName);
+		Application.LoadLevel("Level");
 	}
-
 }
