5269fdde7bdacd5f7948878151180f6cdce361d1
Andrew <empire256@gmail.com>
2014-12-07 20:17:48 +0300

    Added quit script and button action

---
+++ Assets/Scripts/View/GUI/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrappedGame.View.GUI {
+	public class QuitGame : MonoBehaviour {
+		void Start() {
+			gameObject.GetComponent<Button>().onClick.AddListener(() => Quit());
+		}
+
+		public void Quit() {
+			#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+			#else
+				Application.Quit();
+			#endif
+		}
+	}
+}
