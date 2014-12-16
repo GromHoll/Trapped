ec17a6853b53ac7fb13dbd8aa61244157bedaded
Andrew <empire256@gmail.com>
2014-12-16 18:02:41 +0300

    Added reload Level Button

---
+++ Assets/Scripts/View/GUI/LoadLevelScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrappedGame.View.GUI {
+	public class LoadLevelScene : MonoBehaviour {
+		void Start() {
+			gameObject.GetComponent<Button>().onClick.AddListener(() => LoadScene("Level"));
+		}
+		
+		public void LoadScene(string sceneName) {
+			Application.LoadLevel(sceneName);	                       
+		}
+	}
+}
