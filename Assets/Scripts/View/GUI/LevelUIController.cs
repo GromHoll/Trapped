e93a1715edf1dc944eb55249f7814db4978f3cda
GromHoll <gromholl@gmail.com>
2015-01-30 00:02:18 +0300

    rewrite Level UI controller

---
+++ Assets/Scripts/View/GUI/LevelUIController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrappedGame.View.GUI {
+    public class LevelUIController : MonoBehaviour {
+
+
+        public void BackToMainMenu() {
+            LoadScene("MainMenu");
+        }
+
+        public void ReloadLevel() {
+            // TODO I think reload full scene os bad idea
+            LoadScene("Level");
+        }
+
+        private void LoadScene(string sceneName) {
+			Application.LoadLevel(sceneName);	                       
+		}
+	}
+}
