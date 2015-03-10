670c03037d0efee8b3c330db740f13603828134a
GromHoll <gromholl@gmail.com>
2015-03-11 00:12:42 +0300

    Added fixed gen levels and showing win menu after ending turn

--- Assets/Scripts/Main/GameEntry.cs
+++ Assets/Scripts/Main/GameEntry.cs
@@ -60,7 +60,7 @@ namespace TrappedGame.Main {
             if (!game.IsWin()) {
                 UpdateInput();
                 UpdateGraphics();
-            } else {
+            } else if (IsSync()) {
                 ShowWinWindow();
             }
         }
