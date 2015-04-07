c8b2721243821ce7f9ac6de0b22d295ddd39e927
GromHoll <gromholl@gmail.com>
2015-04-07 23:52:36 +0300

    Added win sound

--- Assets/Scripts/View/GUI/WinMenu.cs
+++ Assets/Scripts/View/GUI/WinMenu.cs
@@ -8,11 +8,18 @@ namespace TrappedGame.View.GUI {
 
         public Text scoreField;
         public Text deathField;
+        public AudioClip winAudio;
+
+        private bool isShowed = false;
 
         public void Show(Game game) {
-            gameObject.SetActive(true);
-            scoreField.text = game.GetScore().ToString(CultureInfo.InvariantCulture);
-            deathField.text = game.Hero.DeathCount.ToString(CultureInfo.InvariantCulture);
+            if (!isShowed) {
+                isShowed = true;
+                gameObject.SetActive(true);
+                scoreField.text = game.GetScore().ToString(CultureInfo.InvariantCulture);
+                deathField.text = game.Hero.DeathCount.ToString(CultureInfo.InvariantCulture);
+                AudioSource.PlayClipAtPoint(winAudio, Vector3.zero);
+            }
         }
 
         public void ReturnToMenu() {
